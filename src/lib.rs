5a6ffce6a8230c287e316dbec6af72a46eab9480
Emil <e.roydev@gmail.com>
2025-01-21 21:15:14 +0200

    implemented encapsulated desing
    I have encapsulated my logic inside having only one public function for creation of mneumonic

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,12 @@ const MAX_WORDS: usize = 24;
 pub struct Mnemonic {
     lang: Language,
     mnemonic_type: MnemonicType,
+    entropy: Vec<u8>,
+    checksum: u8,
+    mnemonic_phrase: String,
 }
 
+#[derive(Copy, Clone)]
 pub enum MnemonicType {
     Bits128, // 128 bits of entropy -> 16 bytes (128 bits / 8)
     Bits256, // 256 bits of entropy -> 32 bytes (256 bits / 8)
@@ -52,36 +56,46 @@ pub struct EntropyInfo {
 impl Mnemonic {
 
     pub fn new(lang: Language, mnemonic_type: MnemonicType) -> Mnemonic {
+        let raw_entropy = Mnemonic::generate_entropy(mnemonic_type);
+        let checksum_decimal = Mnemonic::checksum(&raw_entropy, mnemonic_type);
+
         Mnemonic {
             lang,
-            mnemonic_type,
+            mnemonic_type,  // Ownership of `mnemonic_type` is moved here
+            entropy: raw_entropy,  // Pass `mnemonic_type` directly
+            checksum: checksum_decimal,
+            mnemonic_phrase: String::new(),
         }
     }
 
+    pub fn print_mnemonic_data(&self) {
+        println!("Raw Entropy: {:?}, Checksum_decimal: {}", self.entropy, self.checksum);
+    }
 
-    pub fn generate_entropy(&self) -> Vec<u8> {
+    fn generate_entropy(mnemonic_type: MnemonicType) -> Vec<u8> {
         let mut rng = OsRng {};
-        let entropy_bytes_count = self.mnemonic_type.bytes();
+        let entropy_bytes_count = mnemonic_type.bytes();
 
-        let mut entropy = vec![0u8; entropy_bytes_count];// empty vector with [0, 0, 0, 0, 0 ... entropy_size] u8 mean unsigned 8 bit integer
+        let mut entropy = vec![0u8; entropy_bytes_count]; // empty vector
 
         // Fill the vector with random bytes
         rng.fill_bytes(&mut entropy);
         entropy
     }
 
-    pub fn checksum(&self, entropy: &Vec<u8>) -> String {
+
+    fn checksum(entropy: &Vec<u8>, mnemonic_type: MnemonicType) -> u8 {
         let hash = digest(entropy);
 
         if hash.len() < 2 {
             panic!("Hash must be at least 2 characters.");
         }
 
-        let checksum_bits = self.mnemonic_type.bits() / 32;
+        let checksum_bits = mnemonic_type.bits() / 32;
         let checksum_index = if checksum_bits == 4 {1} else if checksum_bits == 8 {2} else {0};
 
-        let checksum = &hash[..checksum_index];
-        String::from(checksum)
+        let checksum = &hash[..checksum_index]; // checksum in hexadecimal
+        u8::from_str_radix(&checksum, 16).expect("Invalid hexadecimal checksum value") // I convert hexadecimal to decimal in order to append in my raw entropy
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,10 @@ mod prelude;
 mod utils;
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
     let mnemonic = Mnemonic::new(Language::English, MnemonicType::Bits256);
-    let entropy = mnemonic.generate_entropy();
-    let checksum_hash = mnemonic.checksum(&entropy);
 
-    println!("{}", checksum_hash);
 
-    println!("{:?}", entropy);
+    mnemonic.print_mnemonic_data();
+
     Ok(())
 }
