2d3ea21493d3a188648b8d59419b18d83608958c
Emil <e.roydev@gmail.com>
2025-01-27 17:32:48 +0200

    added some basic test cases

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,13 +1,22 @@
 use bip39_rusty::{Mnemonic, MnemonicType, Language};
-use super::*;
+
 
 #[test]
-fn test_valid_mnemonic_creation_with_new_method() {
+fn test_valid_mnemonic_creation__returns_mnemonic_instance() {
+    let language = Language::English;
+    let mnemonic_type = MnemonicType::Bits256;
+    let mnemonic = Mnemonic::new(language, mnemonic_type);
+
+    assert!(mnemonic.is_valid());
+}
+
+#[test]
+fn test_invalid_mnemonic_creation__returns_default_mnemonic_instance() {
     let language = Language::English;
     let mnemonic_type = MnemonicType::Bits256;
 
-    let mnemonic = Mnemonic::new(language, mnemonic_type);
-    assert!(mnemonic.is_valid())
+    let mut mnemonic = Mnemonic::new(language, mnemonic_type);
 
 
+    assert!(mnemonic.is_valid());
 }
